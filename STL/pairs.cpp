655caa60623c80eb2fa02163529ee8b25d4493d1
Chidambareswar <chidambareswar02@gmail.com>
2026-06-02 22:49:07 +0530

    Add STL folder

---
+++ STL/pairs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int, int> p = {1, 3};
+    cout << p.first << '\n';
+    cout << p.second << '\n';
+
+    pair<int, pair<int, int>> q = {4, {5, 6}};
+    cout << q.first << endl;
+    cout << q.second.first << endl;
+    cout << q.second.second << endl;
+
+    pair<int, int> arr[] = { {1,2}, {3,4}, {5,6}};
+    cout << arr[2].second;              
+
+}
---
+++ STL/vectors.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v;
+
+    v.push_back(1);
+    v.emplace_back(2);
+}
