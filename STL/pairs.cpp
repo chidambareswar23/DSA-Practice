c3f0bd3580ef21fdaaa5c1547e59a07587b69e82
chidambareswar23 <chidambareswar02@gmail.com>
2026-08-17 14:52:51 +0530

    Add Pairs

--- STL/pairs.cpp
+++ STL/pairs.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    pair<int, int> p = {1, 3};
-    cout << p.first << '\n';
-    cout << p.second << '\n';
+    pair<int, int> p = {1, 3};      
+    cout << p.first << '\n';        
+    cout << p.second << '\n';       
 
-    pair<int, pair<int, int>> q = {4, {5, 6}};
+    pair<int, pair<int, int>> q = {4, {5, 6}};      
     cout << q.first << endl;
     cout << q.second.first << endl;
     cout << q.second.second << endl;
